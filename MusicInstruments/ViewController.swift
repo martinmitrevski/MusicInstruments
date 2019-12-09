ddd846526abdec80e08190b1fd61fef0503ac415
martinmitrevski <martinmitrevski.oh@gmail.com>
2019-12-09 18:24:36 +0100

    implemented music instruments detection

--- MusicInstruments/ViewController.swift
+++ MusicInstruments/ViewController.swift
@@ -14,33 +14,44 @@ class ViewController: UIViewController {
     
     private let audioEngine = AVAudioEngine()
     private var soundClassifier = MySoundClassifier()
-    var inputFormat: AVAudioFormat!
     var streamAnalyzer: SNAudioStreamAnalyzer!
     let queue = DispatchQueue(label: "com.mitrevski.musicinstruments")
+    var results = [(label: String, confidence: Float)]() {
+        didSet {
+            DispatchQueue.main.async { [weak self] in
+                self?.tableView.reloadData()
+            }
+        }
+    }
+    
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.title = "Music instruments"
     }
     
     private func startAudioEngine() {
+        audioEngine.prepare()
         do {
             try audioEngine.start()
+        } catch {
+            showAudioError()
         }
-        catch {
-            fatalError("error starting the audio engine")
+    }
+    
+    private func prepareForRecording() {
+        let inputNode = audioEngine.inputNode
+        let recordingFormat = inputNode.outputFormat(forBus: 0)
+        streamAnalyzer = SNAudioStreamAnalyzer(format: recordingFormat)
+        inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) {
+            [unowned self] (buffer, when) in
+            self.queue.async {
+                self.streamAnalyzer.analyze(buffer,
+                                            atAudioFramePosition: when.sampleTime)
+            }
         }
-        
-        inputFormat = audioEngine.inputNode.inputFormat(forBus: 0)
-        streamAnalyzer = SNAudioStreamAnalyzer(format: inputFormat)
-        
-        audioEngine.inputNode.installTap(onBus: 0, bufferSize: 8000, format: inputFormat) { buffer, time in
-                self.queue.async {
-                    self.streamAnalyzer.analyze(buffer,
-                                                atAudioFramePosition: time.sampleTime)
-                }
-        }
-        
+        startAudioEngine()
     }
     
     private func createClassificationRequest() {
@@ -51,14 +62,74 @@ class ViewController: UIViewController {
             fatalError("error adding the classification request")
         }
     }
+    
+    @IBAction func startRecordingButtonTapped(_ sender: UIButton) {
+        prepareForRecording()
+        createClassificationRequest()
+    }
 
+}
 
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return results.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: "ResultCell")
+        if cell == nil {
+            cell = UITableViewCell(style: .default, reuseIdentifier: "ResultCell")
+        }
+        
+        let result = results[indexPath.row]
+        let label = convert(id: result.label)
+        cell!.textLabel!.text = "\(label): \(result.confidence)"
+        return cell!
+    }
+    
+    private func convert(id: String) -> String {
+        let mapping = ["cel" : "drum", "cla" : "clarinet", "flu" : "flute",
+                       "gac" : "acoustic guitar", "gel" : "electric guitar",
+                       "org" : "organ", "pia" : "piano", "sax" : "saxophone",
+                       "tru" : "trumpet", "vio" : "violin", "voi" : "human voice"]
+        return mapping[id] ?? id
+    }
+    
 }
 
 extension ViewController: SNResultsObserving {
     func request(_ request: SNRequest, didProduce result: SNResult) {
         guard let result = result as? SNClassificationResult else { return }
-        print(result.classifications)
+        var temp = [(label: String, confidence: Float)]()
+        let sorted = result.classifications.sorted { (first, second) -> Bool in
+            return first.confidence > second.confidence
+        }
+        for classification in sorted {
+            let confidence = classification.confidence * 100
+            if confidence > 5 {
+                temp.append((label: classification.identifier, confidence: Float(confidence)))
+            }
+        }
+        results = temp
     }
 }
 
+extension ViewController {
+    
+    func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title,
+                                      message: message,
+                                      preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(action)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func showAudioError() {
+        let errorTitle = "Audio Error"
+        let errorMessage = "Recording is not possible at the moment."
+        self.showAlert(title: errorTitle, message: errorMessage)
+    }
+    
+}
